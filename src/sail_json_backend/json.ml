59d6522f699663fddd906208a819c804cbdd2bd7
Joydeep Tripathy <113792434+crazytrain328@users.noreply.github.com>
2024-07-01 19:35:51 +0530

    feat: removed some whitespaces and line-gaps

--- src/sail_json_backend/json.ml
+++ src/sail_json_backend/json.ml
@@ -620,7 +620,6 @@ let is_reserved_field field_name =
   String.length field_name >= String.length reserved_prefix && 
   String.sub field_name 0 (String.length reserved_prefix) = reserved_prefix 
 
-
 let json_of_field k f = 
   base_string = "{ \"field\": \"" ^ f ^ "\", \"size\": " ^ string_of_sizeof_field k f ^ " }" in
   if is_reserved_field f then
